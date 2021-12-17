e176077c551bbf49d9e6494ee316e1aa0a113a70
bonn1112 <akutsu0521@gmail.com>
2021-12-17 15:57:08 -0500

    add parking_lot and test

--- test/test_parking_lot.py
+++ test/test_parking_lot.py
@@ -13,19 +13,16 @@ class TestParkingLot(unittest.TestCase):
         cls.allocated_slot = 1
 
     def test_create_parking_lot(self):
-        numbers_parking_slots = 6
-        self.parking.create_parking_lot(numbers_parking_slots)
-        self.assertEqual(len(self.parking.slots), numbers_parking_slots, msg="Wrong numbers of parking")
+        self.parking.create_parking_lot(6)
+        self.assertEqual(len(self.parking.slots), 6, msg="Wrong numbers of parking")
 
     def test_park(self):
-        register_number = "MH-12-FF-2017"
-        colour = "Black"
-        self.parking.park(register_number, colour)
+        self.parking.park("MH-12-FF-2017", "Black")
         self.assertFalse(self.parking.slots[self.allocated_slot].is_available, "Park failed.")
         for i in self.parking.slots.values():
             if not i.is_available and i.car:
-                self.assertEqual(i.car.registration_number, register_number, "Park failed")
-                self.assertEqual(i.car.colour, colour, "Park failed")
+                self.assertEqual(i.car.registration_number, "MH-12-FF-2017", "Park failed")
+                self.assertEqual(i.car.colour, "Black", "Park failed")
 
     def test_leave(self):
         self.parking.leave(self.allocated_slot)
